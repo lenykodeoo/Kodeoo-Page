affe41f1928afb7c80526b2e306fe589a2ca8cb3
Leny Folletete Nickel <leny@Mac.lan>
2026-06-02 13:51:25 +0200

    fix CSP bloquer pixel facebook tiers

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,19 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: '**' },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:slug',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; frame-ancestors 'none';",
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
